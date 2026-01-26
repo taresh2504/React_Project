5077ded22ed5c3f0238ac361136f5021b5d2668b
taresh2504 <taresh25202@gmail.com>
2026-01-26 22:11:18 +0530

    validation applied

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,6 +3,7 @@ import ElectricBorder from '../Component/ElectricBorder'
 import '../Tailwind.css'
 import '../App.css'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Signup = () => {
 
@@ -11,6 +12,91 @@ const Signup = () => {
   let login =()=>{
     navigate('/login')
   }
+
+  let handlechange = (e)=>{
+    setform({...form,[e.target.name]:e.target.value})
+  }
+
+  let [form,setform] = useState({
+    myname:'',
+    mynumber:'',
+    myemail:'',
+    mypassword:'',
+    mycpassword:''
+  })
+
+  let valid = true 
+
+  let handlesubmit = (e)=>{
+    e.preventDefault()
+
+    if(form.myname.trim=''){
+      alert('please enter your name')
+      valid = false
+    }
+
+    else if(form.mynumber.trim=''){
+      alert('please enter your contact number')
+      valid = false
+    }
+
+    else if(isNaN(form.mynumber)){
+      alert('please enter your number in digits')
+      valid = false
+    }
+
+    else if(form.myemail.trim=''){
+      alert('please enter your email')
+      valid = false
+    }
+
+    else if(!(form.myemail.includes('@gmail.com')||form.myemail.includes('@yahoo.com')||form.myemail.includes('@outlook.com'))){
+      alert('please enter a valid email')
+      valid = false
+    }
+
+    else if(form.mypassword.trim=''){
+      alert('please enter your password')
+      valid = false
+    }
+
+    else if(!(form.mypassword.match(/[A-Z]/))){
+      alert('password must contain one Capital Letter')
+      valid = false
+    }
+
+    else if(!(form.mypassword.match(/[a-z]/))){
+      alert('password must contain one small letter')
+      valid = false
+    }
+
+    else if(!(form.mypassword.match(/[0-9]/))){
+      alert('password must contain one number')
+    }
+
+    else if(!(form.mypassword.match(/[!@#$%&*-_.`~^/?]/))){
+      alert('password must contain one special character')
+      valid = false
+    }
+
+    else if(!(form.mypassword.length<7 && form.mypassword.length>16)){
+      alert('password is greater than 7 and less than 16 digits')
+      valid = false
+    }
+
+    else if(form.mycpassword.trim=''){
+      alert('please enter your confirm password')
+      valid = false
+    }
+
+    else if(form.mypassword != form.mycpassword){
+      alert('password and confirm password are not same')
+      valid = false
+    }
+
+    if (valid)
+  }
+
 
   return (
     <>
@@ -28,28 +114,28 @@ const Signup = () => {
 >
   <div>
     <p style={{ margin: '6px 0 0', opacity: 0.8 }}>
-      <form className=' ml-8 font-serif'>
+      <form className=' ml-8 font-serif' onSubmit={handlesubmit}>
       <h1 className='ml-38 text-3xl font-serif'>Sign up</h1>
       {/* <br /> */}
       <br />
       <p className='name-head'>Enter Name</p>
-      <input type="text" className='name-box' placeholder='Enter your name' name="" id="" />
+      <input type="text" className='name-box' placeholder='Enter your name' name="myname" value={form.myname} id="" onChange={handlechange} />
       <p></p>
       <br />
       <p className='name-head'>Enter Number</p>
-      <input type="text" className='name-box' placeholder='Enter your number' name="" id="" />
+      <input type="text" className='name-box' placeholder='Enter your number' name="mynumber" value={form.mynumber} id="" onChange={handlechange} />
       <p></p>
       <br />
       <p className='name-head'>Enter Email</p>
-      <input type="email" className='name-box' placeholder='Enter your email' name="" id="" />
+      <input type="email" className='name-box' placeholder='Enter your email' name="myemail" value={form.myemail} id="" onChange={handlechange} />
       <p></p>
       <br />
       <p className='name-head'>Enter Password</p>
-      <input type="Password" className='name-box' placeholder='Enter your Password' name="" id="" />
+      <input type="Password" className='name-box' placeholder='Enter your Password' name="mypassword" value={form.mypassword} id="" onChange={handlechange} />
       <p></p>
       <br />
       <p className='name-head'>Enter Confirm password</p>
-      <input type="password" className='name-box' placeholder='Enter your Confirm password' name="" id="" />
+      <input type="password" className='name-box' placeholder='Enter your Confirm password' name="mycpassword" value={form.mycpassword} id="" onChange={handlechange} />
       <p></p>
       <br />
       <button type="submit" className='signup-button' >Sign up</button>
