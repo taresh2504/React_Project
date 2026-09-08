ab19260e974e33b4c4002340a435dcba0fda34c7
taresh2504 <taresh25202@gmail.com>
2026-09-08 21:54:17 +0530

    update

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -118,7 +118,7 @@ const Signup = () => {
 
         <ElectricBorder
           // color="#7df9ff"
-          color="indigo"
+          color="aqua"
           speed={1}
           chaos={0.12}
           thickness={2}
